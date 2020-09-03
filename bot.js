b37d38d979136bfbe0e41a38b45a4d975d407890
Haque13 <Coreystormkeys13@gmail.com>
2020-09-03 14:59:10 +0700

    fischl

---
+++ bot.js
@@ -0,0 +1,76 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+var nodeSpotifyWebHelper = require('node-spotify-webhelper');
+var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
+var config = require('./config.json');
+var currentSong, newSong;
+
+// Script for exiting the process is from https://frankl.in/code/before-exit-scripts-in-node-js
+let preExit = [];
+
+// Catch exit
+process.stdin.resume();
+process.on('exit', code => {
+  let i;
+  console.log('Process exit');
+  for (i = 0; i < preExit.length; i++) {
+    preExit[i](code);
+  }
+  client.user.setGame(null);
+  process.exit(code);
+});
+
+// Catch CTRL+C
+process.on('SIGINT', () => {
+  console.log('\nCTRL+C...');
+  process.exit(0);
+});
+
+// Catch uncaught exception
+process.on('uncaughtException', err => {
+  console.dir(err, {
+    depth: null
+  });
+  process.exit(1);
+});
+
+// INSERT CODE
+console.log('Bot on, hit CTRL+C to exit :)');
+
+// Add pre-exit script
+preExit.push(code => {
+  console.log('Whoa! Exit code %d, cleaning up...', code);
+});
+
+//Initially sets your game to the current song
+spotify.getStatus(function(err, res) { 
+  if (err) {
+    return console.error(err);
+  }
+  console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
+
+  currentSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
+  client.user.setGame(currentSong);
+});
+
+//This Updates the song every 10 seconds
+setInterval(function(err, res) { 
+  spotify.getStatus(function(err, res) {
+    if (err) {
+      return console.error(err);
+    }
+    newSong = res.track.track_resource.name + ' - ' + res.track.artist_resource.name;
+    if (newSong != currentSong) {
+      client.user.setGame(newSong);
+      console.info('currently playing:', res.track.artist_resource.name, '-', res.track.track_resource.name);
+      currentSong = newSong;
+    }
+  });
+}, 10000/*Change the value here to change how often the song is updated. This number is in miliseconds*/); 
+
+process.on('unhandledRejection', error => {
+  console.error(`Uncaught Promise Error: \n${error.stack}`);
+});
+
+client.login(config.token)
