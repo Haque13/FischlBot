81743da852e924f5819b014c54f9be906b3e7661
Haque13 <Coreystormkeys13@gmail.com>
2020-09-03 21:07:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,14 +26,28 @@ bot.on("message", async (message) => { // eslint-disable-line
     let command = message.content.toLowerCase().split(" ")[0];
     command = command.slice(PREFIX.length);
 
-    if (message.content.startsWith(PREFIX+'avatar')) {
-        const user = message.mentions.users.first() || message.author;
-        const avatarEmbed = new bot.RichEmbed()
-            .setColor(0x333333)
-            .setAuthor(user.username)
-            .setImage(user.avatarURL);
-        message.channel.send(avatarEmbed);
+    if (message.content.startsWith(PREFIX + 'avatar')) {
+    
+        let embed = new MessageEmbed();
+    if(!message.mentions.users.first()) {
+        embed.setTitle('Your avatar');
+        embed.setDescription(`Links:\n[png](${message.author.displayAvatarURL({format: "png", size: 2048})}) | [jpg](${message.author.displayAvatarURL({format: "jpg", size: 2048})}) | [gif](${message.author.displayAvatarURL({format: "gif", size: 2048, dynamic: true})}) | [webp](${message.author.displayAvatarURL({format: "webp", size: 2048})})`);
+        embed.setColor(0x00008b);
+        embed.setTimestamp();
+        embed.setFooter(message.author.username);
+        embed.setImage(message.author.displayAvatarURL({size: 2048, dynamic: true}));
+        message.channel.send(embed);
+    } else {
+        let user = message.mentions.users.first();
+        embed.setTitle(`‹${user.username}›'s avatar`);
+        embed.setDescription(`Links:\n[png](${user.displayAvatarURL({format: "png", size: 2048})}) | [jpg](${user.displayAvatarURL({format: "jpg", size: 2048})}) | [gif](${user.displayAvatarURL({format: "gif", size: 2048, dynamic: true})}) | [webp](${user.displayAvatarURL({format: "webp", size: 2048})})`);
+        embed.setColor(0x8b0000);
+        embed.setTimestamp();
+        embed.setFooter(user.username);
+        embed.setImage(bot.users.cache.get(user.id).displayAvatarURL({size: 2048, dynamic: true}));
+        message.channel.send(embed);
     }
+}
 })
 
 bot.login(process.env.BOT_TOKEN);
