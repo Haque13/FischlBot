a31385a830485cc66c4871d4fc5033bc8d4842a4
Haque13 <Coreystormkeys13@gmail.com>
2020-09-04 13:46:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ bot.on("message", async (message) => { // eslint-disable-line
     
     let command = message.content.toLowerCase().split(" ")[0];
         command = command.slice(prefix1.length);
-    let command = message.mentions.users.first();
+        command = message.mentions.users.first();
 
         let embed = new MessageEmbed();
     if (command === "avatar" || command === "profile") {
