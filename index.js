68afb5565396410a97bd40d032518771a44cb7d7
Haque13 <Coreystormkeys13@gmail.com>
2020-09-04 14:38:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,10 @@ bot.on("message", async (message) => { // eslint-disable-line
     const searchString = args.slice(1).join(" ");
     const url = args[1] ? args[1].replace(/<(.+)>/g, "$1") : "";
     
-    let command = message.content.toLowerCase().split(" ")[0];
-        command = command.slice(prefix1.length || prefix2.length);
         
         let embed = new MessageEmbed(); {
-        let command = message.mentions.users.first()
-    if (command === "help" || command === "cmd") { 
+    if  (!message.content.toLowerCase().startsWith(prefix1 + 'avatar')) { 
+        (!message.mentions.users.first())
         message.channel.send("Aku tidak bisa melakukan pemanggilan, jika tidak mengetahui siapa yg harus ku panggil!")       
 }  else {
         let user = message.mentions.users.first();
