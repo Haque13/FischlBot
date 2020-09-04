97e211e60112a4886b284aa99bec3bbff575de24
Haque13 <Coreystormkeys13@gmail.com>
2020-09-04 23:16:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const Discord, { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const client = new Discord.Client();
 require("dotenv").config();
 
